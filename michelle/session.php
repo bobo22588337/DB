1b365ba1c1914de3a6c2bcb8b7e0aeeb2758bc7d
michelle-hsieh <michelleok168@gmail.com>
2020-12-30 21:15:36 +0800

    upload session login logout register (errors exist)

--- michelle/session.php
+++ michelle/session.php
@@ -2,8 +2,8 @@
 session_start();
 
 // initializing variables
-$user_email = "";
-$email    = "";
+/*$user_name = "";
+$user_email = "";*/
 $errors = array(); 
 
 // connect to the database
@@ -13,23 +13,31 @@ mysql_select_db("dessert",$db);
 
 /************* register ************/
 if (isset($_POST['register'])) {
-  // receive all input values from the form
-  $username = mysql_real_escape_string($_POST['user_email']);
-  $password_1 = mysql_real_escape_string($_POST['password1']);
-  $password_2 = mysql_real_escape_string($_POST['password2']);
+    // receive all input values from the form
+    /*$user_name = mysql_real_escape_string($_POST['user_name']);
+    $user_email = mysql_real_escape_string($_POST['user_email']);
+    $password_1 = mysql_real_escape_string($_POST['password1']);
+    $password_2 = mysql_real_escape_string($_POST['password2']);*/
+    $user_name = $_POST['user_name'];
+    $user_email = $_POST['user_email'];
+    $password_1 = $_POST['password1'];
+    $password_2 = $_POST['password2'];
   
-  // form validation: ensure that the form is correctly filled ...
-  // by adding (array_push()) corresponding error unto $errors array
-  if (empty($user_email)) {
-	  array_push($errors, "請輸入帳號"); 
-	  }
-  if (empty($password_1)) { 
-	  array_push($errors, "請輸入密碼");
-	  }
+    // form validation: ensure that the form is correctly filled ...
+    // by adding (array_push()) corresponding error unto $errors array
+    if (empty($user_name)) {
+    array_push($errors, "請輸入姓名／暱稱"); 
+     }
+    if (empty($user_email)) {
+    array_push($errors, "請輸入帳號"); 
+    }
+    if (empty($password_1)) { 
+    array_push($errors, "請輸入密碼");
+    }
 
-  if ($password_1 != $password_2) {
-	array_push($errors, "密碼不一致");
-  }
+    if ($password_1 != $password_2) {
+    array_push($errors, "密碼不一致");
+    }
 
   // first check the database to make sure 
   // a user does not already exist with the same username and/or email
@@ -46,17 +54,15 @@ if (isset($_POST['register'])) {
 
   // Finally, register user if there are no errors in the form
   if (count($errors) == 0) {
-	  $password = $password_1;
-	  mysql_query("SET NAMES 'utf8'");
-	  $query1 = "insert into user (user_email, user_psw, identify, status) 
-  			  values('$user_email', '$password', 'user', '正常')";
-	  mysql_query($query1,$db);
-	
-	echo "<script language='JavaScript'>alert('註冊成功',location.href='index_.php')</script>";
+    $password = $password_1;
+    mysql_query("SET NAMES 'utf8'");
+    $query1 = "insert into user (user_name, user_email, user_psw, identity, status) values('$user_name', $user_email', '$password', 'user', 1)";
+    mysql_query($query1,$db);
+
+    echo "<script language='JavaScript'>alert('註冊成功',location.href='../index.html')</script>";
     //這一行重新導向的位置待改
-		
-  	$_SESSION['user_email'] = $username;
-	
+
+    $_SESSION['user_email'] = $user_email;
   }
 }
 
@@ -75,24 +81,24 @@ if (isset($_POST['login'])) {
   if (count($errors) == 0) {
 	mysql_query( 'set names utf8' );
 
-  	$query = "SELECT * FROM user WHERE user_email = '$user_email' AND password='$password' AND identify = 'user' AND status = '正常'";
+  	$query = "SELECT * FROM user WHERE user_email = '$user_email' AND user_psw='$password' AND identity = 'user' AND status = 1";
   	$results = mysql_query($query,$db);
 
   	if (mysql_num_rows($results) == 1) {
 		$_SESSION['user_email'] = $user_email;
-		header("location:index_.php"); //此處導向待改
+        header('location: ../index.html'); //此處導向待改
 	}
   	else{
-		echo "<script language='JavaScript'>alert('你已被停權',location.href='index_.php')</script>";
+		echo "<script language='JavaScript'>alert('你已被停權',location.href='../index.html')</script>"; //此處導向待改
 	}
   	
 	
-/***manager  ***/
-	$sql = "SELECT * FROM user WHERE user_email='$user_email' AND password='$password' AND identify = 'manager'";
+/*** manager  ***/
+	$sql = "SELECT * FROM user WHERE user_email='$user_email' AND user_psw='$password' AND identity = 'admin' AND status = 1";
 	$re = mysql_query($sql,$db);
 	if(mysql_num_rows($re) == 1){
 		$_SESSION['user_email'] = $user_email;
-		header('location:../admin/administrator.php'); //此處導向待改
+		header('location: ../index.html'); //此處導向待改
 	}
 
 	else {
